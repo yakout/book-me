d80f334afa7796fedc6d96b2f955f6d7fb74a7a9
MuhammedKhamis <muhammedessam10@gmail.com>
2018-06-05 23:12:14 +0200

    adding annotations instead of servlet in web.xml

--- src/main/java/servlets/BookSearch.java
+++ src/main/java/servlets/BookSearch.java
@@ -1,11 +1,14 @@
 package servlets;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+
+@WebServlet(name = "booksearch", urlPatterns = "/booksearch")
 public class BookSearch extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
--- src/main/java/servlets/CheckOut.java
+++ src/main/java/servlets/CheckOut.java
@@ -3,11 +3,13 @@ package servlets;
 import beans.Cart;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet(name = "checkout", urlPatterns = "/checkout")
 public class CheckOut extends HttpServlet {
 
 
