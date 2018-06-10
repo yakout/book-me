4a1288cfba63b8c233bf66940f66db1695289e71
Ahmed Yakout <iyakout@hotmail.com>
2018-06-10 02:05:51 +0200

    just made the jasper code general

--- src/main/java/servlets/Statistics.java
+++ src/main/java/servlets/Statistics.java
@@ -28,32 +28,14 @@ public class Statistics extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String reportPath = "/ReportTemplates/";
         String file_name = "";
+        String query;
         if (request.getParameter("stat1") != null) {
             System.out.print("stat1");
             file_name = "total_sales.jrxml";
 
             // THIS QUERY WORKS
-            String query = "SELECT SUM(Sale.copies * price) from Sale Join book on (Sale.ISBN = Book.ISBN) " +
+            query = "SELECT SUM(Sale.copies * price) from Sale Join book on (Sale.ISBN = Book.ISBN) " +
                     "WHERE sale_date > (current_Date() - Interval 1 MONTH);";
-
-            JasperReportBuilder totalSales = DynamicReports.report();
-            totalSales
-                    .columns(Columns.column("Total Profit Last Month", "SUM(Sale.copies * price)",
-                                    DataTypes.stringType()))
-                    .title(Components.text("Total Profit Last Month")
-                            .setHorizontalAlignment(CENTER))
-                    .pageFooter(Components.pageXofY())
-                    .setDataSource(query, ModelManager.getInstance().getConnection());
-            try {
-                totalSales.show();
-
-                response.setContentType("application/pdf");
-                totalSales.toPdf(response.getOutputStream());
-                response.getOutputStream().flush();
-                response.getOutputStream().close();
-            } catch (Exception e1) {
-                 e1.printStackTrace();
-            }
         }
         else if (request.getParameter("stat2") != null) {
             System.out.print("stat2");
@@ -63,6 +45,26 @@ public class Statistics extends HttpServlet {
             System.out.print("stat3");
             file_name = "TopTenBooks.jrxml";
         }
+
+        JasperReportBuilder totalSales = DynamicReports.report();
+        totalSales
+                .columns(Columns.column("Total Profit Last Month", "SUM(Sale.copies * price)",
+                        DataTypes.stringType()))
+                .title(Components.text("Total Profit Last Month")
+                        .setHorizontalAlignment(CENTER))
+                .pageFooter(Components.pageXofY())
+                .setDataSource(query, ModelManager.getInstance().getConnection());
+        try {
+            totalSales.show();
+
+            response.setContentType("application/pdf");
+            totalSales.toPdf(response.getOutputStream());
+            response.getOutputStream().flush();
+            response.getOutputStream().close();
+        } catch (Exception e1) {
+            e1.printStackTrace();
+        }
+
 //        prepare_report(response,file_name,reportPath+file_name);
 //        request.getRequestDispatcher("statistics.jsp").forward(request, response);
     }
